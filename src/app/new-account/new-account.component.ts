8bb617ade369d60f5f5ed6ebd39f2f7310b85395
larryvsdata <larryvsdata@gmail.com>
2019-06-06 10:14:11 -0500

    Service Refactored

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -3,10 +3,15 @@ import {LoggingService} from '../logging.service';
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  styleUrls: ['./new-account.component.css'],
+  providers: [LoggingService]
 })
 export class NewAccountComponent {
   @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+
+  constructor(private loggingService: LoggingService){
+
+  }
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountAdded.emit({
@@ -14,6 +19,6 @@ export class NewAccountComponent {
       status: accountStatus
     });
     const service = new LoggingService();
-    service.logStatusChange(accountStatus);
+    this.loggingService.logStatusChange(accountStatus);
   }
 }
